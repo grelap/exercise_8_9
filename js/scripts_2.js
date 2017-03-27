69ce50a252eea415f4acb0ec0ceff22692109779
xenogonx <pablez@wp.pl>
2017-03-27 19:55:42 +0200

    comments_for_working_tree

--- js/scripts_2.js
+++ js/scripts_2.js
@@ -1,9 +1,12 @@
  function rysujChoinke (rows) {
+   
   for (var i = 1; i <= rows; i++){ 
     var star = "";
+      //tutaj pętla która przelicza odstęp od lewej krawędzi
       for (var c = 0; c < rows-i; c++){
             star +=' ';
           }
+      //a tutaj pętla przeliczająca liczbę gwiazdek
       for (var n = 0; n < i*2-1; n++){ 
          star += '*';     
       } 
